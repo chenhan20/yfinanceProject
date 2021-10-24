51c43bb4d188e478b49850ec42d4a223b2094cee
ZHUANG,CHUANG-HAN(Steve) <a0911558945@gmail.com>
2021-10-24 05:29:48 +0000

    取得股價與最後更新時間

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import yfinance as yf
 
 
 ticker = "TSLA"
+
 msft = yf.Ticker(ticker)
+stock_info = yf.Ticker(ticker).info
 
 # get stock info
 ticker_yahoo = yf.Ticker(ticker)
 data = ticker_yahoo.history()
 last_quote = (data.tail(1)['Close'].iloc[0])
-print(data.tail(1)['Close'])
+
+# market_price = stock_info['regularMarketPrice']
+# previous_close_price = stock_info['regularMarketPreviousClose']
+# print('market price ', market_price)
+# print('previous close price ', previous_close_price)
+
+print('Date:',str(data.tail(1)['Close'].index.values[0]))
 print(ticker,last_quote)
